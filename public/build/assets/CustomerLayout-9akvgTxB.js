e69e3ed06718ccf5c34a49460098e5538c45dba6
horlaryehmee <horlaryehmee@gmail.com>
2026-08-09 14:13:28 +0100

    Build production frontend assets

---
+++ public/build/assets/CustomerLayout-9akvgTxB.js
@@ -0,0 +1,1 @@
+import{j as o,D as t,a4 as r}from"./main-xT44fEOL.js";function e(a){return o.jsx(t,{navigation:r,role:"customer",...a})}export{e as default};
--- public/build/assets/CustomerLayout-CnacOvZr.js
+++
@@ -1,1 +0,0 @@
-import{j as o,D as t,a4 as r}from"./main-BGyb9hju.js";function e(a){return o.jsx(t,{navigation:r,role:"customer",...a})}export{e as default};
